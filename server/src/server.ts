30d35f64c20e766ee695de7cab27980f3010a1f8
eduvzr <eduviezzer@gmail.com>
2023-01-16 21:57:13 -0300

    adicionado orm prisma

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,13 @@
-console.log("oi")
+import Fastify from 'fastify'
+
+const app = Fastify()
+
+app.get('/', () => {
+    return 'Olá mundo'
+})
+
+app.listen({
+    port: 3333
+}).then(() => {
+    console.log('Servidor HTTP rodando!')
+})
