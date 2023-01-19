a546a2c34d1cd88db33a6ebc257755d39fb49b2b
eduvzr <eduviezzer@gmail.com>
2023-01-19 12:25:16 -0300

    criado tabelas do banco

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,15 @@
+import './styles/global.css'
+
 import { Habit } from "./components/Habit"
 
 function App() {
   return (
-    <Habit completed={3}/>
+    <div>
+      <Habit completed={3}/>
+      <Habit completed={4}/>
+      <Habit completed={5}/>
+      <Habit completed={6}/>
+    </div>
   )
 }
 
