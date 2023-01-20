2d84877ea9b5625f5b9db376ce2bcd9a02344728
eduvzr <eduviezzer@gmail.com>
2023-01-20 12:33:24 -0300

    criado novos componentes

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -25,15 +25,15 @@ export function SummaryTable() {
 
             <div className="grid grid-rows-7 grid-flow-col gap-3">
                 {summaryDates.map(date => {
-                    return (
-                        <HabitDay key={date.toString()} />
-                    )
+                    return <HabitDay
+                            key={date.toString()}
+                            amount={5}
+                            completed={Math.random()*5}
+                    />
                 })}
 
                 {amountOfDaysToFill > 0 && Array.from({length: amountOfDaysToFill}).map((_, i) => {
-                    return (
-                        <div key={i} className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursosr-not-allowed"/>
-                    )
+                    return <div key={i} className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursosr-not-allowed"/>
                 })}
             </div>
         </div>
